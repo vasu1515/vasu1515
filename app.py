86e74ff57b73155e4db7bf02bd2167a7bcc353a9
vasu1515 <vasu@alumni.esade.edu>
2019-12-23 16:08:51 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,171 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+
+# app = dash.Dash()
+app = dash.Dash(_name_)
+server=app.server
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
+
+df = pd.read_csv('nama_10_gdp_1_Data.csv')
+
+available_indicators = df['NA_ITEM'].unique()
+format_figures = df['UNIT'].unique()
+country_indicators = df['GEO'].unique()
+
+app.layout = html.Div([
+    html.Div([
+
+        html.Div([
+            dcc.Dropdown(
+                id='xaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Gross domestic product at market prices'
+            ),
+            dcc.RadioItems(
+                id='xaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            ),
+            dcc.RadioItems(
+                id='value-format',
+                options=[{'label': i, 'value': i} for i in format_figures],
+                value='Chain linked volumes, index 2010=100',
+                labelStyle={'display': 'inline-block'}
+            )
+        ],
+        style={'width': '48%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                id='yaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Final consumption expenditure'
+            ),
+            dcc.RadioItems(
+                id='yaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+    ]),
+
+    dcc.Graph(id='indicator-graphic'),
+
+    dcc.Slider(
+        id='year--slider',
+        min=df['TIME'].min(),
+        max=df['TIME'].max(),
+        value=df['TIME'].max(),
+        step=None,
+        marks={str(year): str(year) for year in df['TIME'].unique()}
+    ),
+
+    html.Hr(),
+
+    html.Div([
+        html.Div([
+            dcc.Dropdown(
+                id='yaxis-column2',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Gross domestic product at market prices'
+            ),
+            dcc.RadioItems(
+                id='yaxis-type2',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            ),
+            dcc.RadioItems(
+                id='value-format2',
+                options=[{'label': i, 'value': i} for i in format_figures],
+                value='Chain linked volumes, index 2010=100',
+                labelStyle={'display': 'inline-block'}
+            )
+        ],
+        style={'width': '48%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                id='country-select',
+                options=[{'label': i, 'value': i} for i in country_indicators],
+                value='Iceland'
+            )
+            ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+    ]),
+
+    dcc.Graph(id='lineplot-graphic')
+])
+
+@app.callback(
+    dash.dependencies.Output('indicator-graphic', 'figure'),
+    [dash.dependencies.Input('xaxis-column', 'value'),
+     dash.dependencies.Input('yaxis-column', 'value'),
+     dash.dependencies.Input('xaxis-type', 'value'),
+     dash.dependencies.Input('yaxis-type', 'value'),
+     dash.dependencies.Input('value-format', 'value'),
+     dash.dependencies.Input('year--slider', 'value')])
+def update_graph(xaxis_column_name, yaxis_column_name,
+                 xaxis_type, yaxis_type, value_format,
+                 year_value):
+    dff = df[(df['TIME'] == year_value) & (df['UNIT']==value_format)]
+
+    return {
+        'data': [go.Scatter(
+            x=dff[dff['NA_ITEM'] == xaxis_column_name]['Value'],
+            y=dff[dff['NA_ITEM'] == yaxis_column_name]['Value'],
+            text=dff[dff['NA_ITEM'] == yaxis_column_name]['GEO'],
+            mode='markers',
+            marker={
+                'size': 15,
+                'opacity': 0.5,
+                'line': {'width': 0.5, 'color': 'white'}
+            }
+        )],
+        'layout': go.Layout(
+            xaxis={
+                'title': xaxis_column_name,
+                'type': 'linear' if xaxis_type == 'Linear' else 'log'
+            },
+            yaxis={
+                'title': yaxis_column_name,
+                'type': 'linear' if yaxis_type == 'Linear' else 'log'
+            },
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+            hovermode='closest'
+        )
+    }
+
+@app.callback(
+    dash.dependencies.Output('lineplot-graphic', 'figure'),
+    [dash.dependencies.Input('yaxis-column2', 'value'),
+     dash.dependencies.Input('yaxis-type2', 'value'),
+     dash.dependencies.Input('country-select', 'value'),
+     dash.dependencies.Input('value-format2', 'value')])
+def update_graph1(yaxis_column, yaxis_type, country_value, value_format):
+    dff = df[(df['NA_ITEM'] == yaxis_column) & (df['UNIT'] == value_format) & (df['GEO']== country_value)]
+
+    return {
+        'data': [go.Scatter(
+            x=dff.TIME,
+            y=dff.Value,
+            text=dff.GEO,
+            mode='lines'
+        )],
+        'layout': go.Layout(
+            yaxis={
+                'title': yaxis_column,
+                'type': 'linear' if yaxis_type == 'Linear' else 'log'
+            },
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+            hovermode='closest'
+        )
+    }
+
+if _name_ == '_main_':
+    app.run_server()
